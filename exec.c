8ac7564d9bb7676f04b37e2a474d049b217160a5
Evan Lee <theazion@gmail.com>
2015-09-24 17:59:13 -0400

    Added additional execution functions.
    * Added support to convert command structs into command buffers for the purposes of execv.
    
    * Functionalized processCommand in order to allow etime to process and run its subcommand.
    
    * Added copy struct functionality.

--- exec.c
+++ exec.c
@@ -4,7 +4,45 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void executeCommand(Commmand c){
+void copyStruct(cmdStruct dest, cmdStruct src){
+	strcpy(dest.cmd, src.cmd);
+	int i;
+	int l = sizeof(src.args)/sizeof(char*);
+	for(i = 0; i < l; i++) {
+		strcpy(dest.args[i], src.args[i]);
+	}
+	if(strcmp(src.rdFile, "") != 0){
+		strcpy(dest.rdFile, src.rdFile);
+	}
+	dest.rd = src.rd;
+	dest.bg = src.bg;
+}
+
+void processCommand(cmdStruct c){
+	if(strcmp(c.cmd, "cd") == 0){
+		cd(c);
+	}
+	else if(strcmp(c.cmd, "echo") == 0){
+		echo (c);
+	}
+	else if(strcmp(c.cmd, "etime") == 0){
+		etime(c);
+	}
+	else if(strcmp(c.cmd, "limits") == 0){
+		limits(c);
+	}
+	else
+		executeCommand(c);
+}
+
+char* buildCmdFromStruct(cmdStruct c){
+	char* cmd = (char*) malloc (sizeof(c.cmd)+sizeof(c.args)+1);
+	strcat(cmd, c.cmd);
+	strcat(cmd, c.args);
+	return cmd;
+}
+
+void executeCommand(cmdStruct c){
 	int status;
 	pid_t p = fork();
 	int bg = c.bg;
@@ -24,7 +62,8 @@ void executeCommand(Commmand c){
 			dup(rdFD);
 			close(rdFD);
 		}
-		execv(cmd[0], cmd);
+		char* cmdBuff = buildCmdFromStruct(c);
+		execv(c.cmd, cmdBuff);
 		exit(1);
 	}
 	// PARENT
@@ -34,10 +73,3 @@ void executeCommand(Commmand c){
 	}
 }
 
-int main() {
-	char* cmd[3] = {"/bin/ls", "-l", NULL};
-	puts("I will execute the command.");
-	executeCommand(cmd);
-	puts("I have executed the command.");
-	return 0;
-}

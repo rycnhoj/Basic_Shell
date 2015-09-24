f6df596c8d9b014bff2281f2fba6656c329c566f
rycnhoj <jrc11v@my.fsu.edu>
2015-09-24 17:36:29 -0400

    Added Struct

--- main_j.c
+++ main_j.c
@@ -1,9 +1,3 @@
-// http://www.lemoda.net/c/recursive-directory/
-
-
-// #include <unistd.h>
-// #include <pwd.h>
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +5,7 @@
 #include <dirent.h>
 #include <errno.h>
 
-int MAX_ARGS = 256;
+#define MAX 256
 
 static char* getcmd(void) {
 
@@ -39,19 +33,19 @@ static char* getcmd(void) {
 	return buf;
 }
 
-int changeEnvs(char * args[], int arraySize) {
+int changeEnvs(char * tokenArray[], int arraySize) {
 	int i = 0;
 
 	while(i < arraySize + 1) {
-		if(args[i][0] == '$') {
+		if(tokenArray[i][0] == '$') {
 
 			char* env;
-			env = (char*) malloc(strlen(args[i]) - 1); 
+			env = (char*) malloc(strlen(tokenArray[i]) - 1); 
 
-			strncpy(env, args[i]+1, strlen(args[i])); // Remove '$'
+			strncpy(env, tokenArray[i]+1, strlen(tokenArray[i])); // Remove '$'
 
 			if (getenv(env) != NULL) { // Replace the value if it exists
-				args[i] = getenv(env);
+				tokenArray[i] = getenv(env);
 			} else { // Signal an error if it does not exist
 				printf("%s: Undefined Variable.\n", env);
 				return 1;
@@ -69,26 +63,20 @@ int changeEnvs(char * args[], int arraySize) {
 //Execute command
 //Print results
 
-
 // - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+typedef struct {
+	char * cmd;
+	char * args[MAX];
+	char * rdFile;
+
+	int T;
+	int bg;
+
+} cmdStruct;
 
 int main() {
 
-	// FILE *fp;
-	// int c;
-
-	// fp = fopen("file.txt","r");
-	// while(1) {
-	// 	c = fgetc(fp);
-	// 	if(feof(fp)) { 
-	// 		break;
-	// 	}
-		
-	// 	printf("%c", c);
-	// }
-
-	// fclose(fp);
-	
 	char* cmdline; // The Whole Command Line
 
 	while ((cmdline = getcmd()) != NULL) {
@@ -96,82 +84,124 @@ int main() {
 		char* token; 
 		char* rest = cmdline;
 
-		char* args[MAX_ARGS];
+		char* tokenArray[MAX];
+
+		cmdStruct cmdStructs[MAX];
 		int argIndex = -1;
 
 		//Transform input
 		/////////////////////////////////////////////////
 
-		// Load all the command tokens into args array
+		// Load all the command tokens into tokenArray array
 		while(token = strtok_r(rest, " ", &rest)) {
-			args[++argIndex] = token;
+			tokenArray[++argIndex] = token;
 		}
 
+		// while(not end of token_array)
+		// create temp Struct
+		// put first token into Struct.cmd
+		// while(not "|" or NULL)
+		// 	if(token == <)
+		// 		Struct.redirect = 0
+		// 	else if(token == >)
+		// 		Struct.redirect = 1
+		// 	else if(token == &)
+		// 		Struct.bg = true
+		// 	else
+		// 		copy token into Struct.args
+		// push temp Struct into array of structs
+
+		int p = 0;
+		int cmdStructIndex = 0;
+
+		cmdStruct * temp;
+
+		while(tokenArray[p] != NULL) {
+			temp = cmdStructs[cmdStructIndex++];
+
+			temp.cmd = tokenArray[p]
+
+			while(tokenArray[p] != NULL || tokenArray[p] != '|'){
+				if (strcmp(tokenArray[p], "<") == 0)
+				{
+					temp.rd = 0;
+				} else if (strcmp(tokenArray[p], "<") == 0) {
+
+
+				} else {
+
+
+
+				}
+			}
+
+			p++;
+		}
+
+
 		// Checks all the enviromental variables
-		// if(changeEnvs(args, argIndex) == 0) {
+		// if(changeEnvs(tokenArray, argIndex) == 0) {
 
 			//Match against patterns
 			/////////////////////////////////////////////////
 
 			// CLEAR
 			/////////////////////////////////////////////////
-			if(!strcmp(args[0], "clear")) 
-				system("clear");
+			// if(!strcmp(tokenArray[0], "clear")) 
+			// 	system("clear");
 
 			// EXIT
 			/////////////////////////////////////////////////
-			if(!strcmp(args[0], "exit")) {
-				printf("Exiting Shell....\n");
-				return 0;
-			}
+			// if(!strcmp(tokenArray[0], "exit")) {
+			// 	printf("Exiting Shell....\n");
+			// 	return 0;
+			// }
 
 			// ECHO
 			/////////////////////////////////////////////////
 			// Outputs whatever the user specifies
 
-			if(!strcmp(args[0], "echo")) {
-				int k = 0;
+			// if(!strcmp(tokenArray[0], "echo")) {
+			// 	int k = 0;
 
-				// For each argument passed to echo	print		
-				while(k < argIndex) { printf("%s ", args[++k]); }
+			// 	// For each argument passed to echo	print		
+			// 	while(k < argIndex) { printf("%s ", tokenArray[++k]); }
 
-				printf("\n");
-			}
+			// 	printf("\n");
+			// }
 
 			// // LS
 			// /////////////////////////////////////////////////
-			// else if(!strcmp(args[argIndex], "ls")) { 
+			// else if(!strcmp(tokenArray[argIndex], "ls")) { 
 
 			// }
 
 			// // MORE
 			// /////////////////////////////////////////////////
-			// else if(!strcmp(args[argIndex], "more")) { 
+			// else if(!strcmp(tokenArray[argIndex], "more")) { 
 		
 			// }
 
 			// CD
 			/////////////////////////////////////////////////
-			// else if(!strcmp(args[0], "cd")) { 
-			// 	changeDir(args);
+			// else if(!strcmp(tokenArray[0], "cd")) { 
+			// 	changeDir(tokenArray);
 			// }
 
 			/////////////////////////////////////////////////	
 			
-		}
+		// }
 
 		//Cleanup
 		/////////////////////////////////////////////////	
 
-		int j = 0;
+		// int j = 0;
 		
-		while(j < 256) {
-			args[j] = 0;
-			j++;
-		}
+		// while(j < 256) {
+		// 	tokenArray[j] = 0;
+		// 	j++;
+		// }
 	}
 
 	return 0;
 }
-
-

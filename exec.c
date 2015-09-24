cf62208497fc24c11b6409efcbe3e915655419f1
Evan Lee <theazion@gmail.com>
2015-09-24 15:29:32 -0400

    Finished redirection.

--- exec.c
+++ exec.c
@@ -4,16 +4,26 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void executeCommand(char* cmd[]){
+void executeCommand(Commmand c){
 	int status;
 	pid_t p = fork();
-	bool bg = false;
+	int bg = c.bg;
 	if(p == -1){
 		fprintf(stderr, "A forking error ocurred.\n");
 		exit(EXIT_FAILURE);
 	}
 	// CHILD
 	else if (p == 0) {
+		// REDIRECTION
+		if(c.redirect != -1){
+			int rdFD = open(fileno(c.rdfile));
+			if(c.redirect == 0)
+				close(STDIN_FILENO);
+			else if(c.redirect == 1)
+				close(STDOUT_FILENO);
+			dup(rdFD);
+			close(rdFD);
+		}
 		execv(cmd[0], cmd);
 		exit(1);
 	}

747963eeded37fddecb96474c10287d23346276c
John Cyr <rycnhoj@gmail.com>
2016-01-16 15:50:39 -0500

    Update define.h

--- define.h
+++ define.h
@@ -4,21 +4,22 @@
 	the command struct in order to
 	span multiple source code files.
 	================================ **/
+	
 #ifndef DEFINE_H
 #define DEFINE_H
 
 typedef struct {
-	char* cmd;	// String of the command
-	char* args[256];  // Array of argument strings
-	char* rdFile;  // String holding the name of the file to redirect to
+	char* cmd;		// String of the command
+	char* args[256];  	// Array of argument strings
+	char* rdFile;  		// String holding the name of the file to redirect to
 
 	int rd;
-		// -1 for not redirected
-		// 0 for '>'
-		// 1 for '<'
+				// -1 for not redirected
+				// 0 for '>'
+				// 1 for '<'
 	int bg;
-		// -1 for not backgrounded
-		// 1 for backgrounded
+				// -1 for not backgrounded
+				// 1 for backgrounded
 } cmdStruct;
 
 #endif
